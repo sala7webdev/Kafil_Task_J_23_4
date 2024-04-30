7794a931177425f7a57a774380a72c751a84f2ef
sala7webdev <mohamed.salah.webdev@gmail.com>
2024-04-30 14:30:07 +0300

    optimizing SEO and some performance issues

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,14 @@
+
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
-const concat = require('gulp-concat');
 const { series } = require('gulp');
 const replace = require('gulp-replace');
+
 const fs = require('fs');
 const path = require('path');
+var uglifycss = require('gulp-uglifycss');
+const uglify = require('gulp-minify');
+
 
 
 
@@ -17,17 +21,33 @@ function buildHTML() {
       }
       return match; // If file doesn't exist, return the original placeholder
     }))
+
     .pipe(gulp.dest('dist')); // Output directory
 };
 
 
+/*************
+ * Scss Files
+ ***********/
 
-function combilecss() {
+function combileAndMinifyCSS() {
   return gulp.src('./src/scss/**/*.scss')
     .pipe(sass().on('error', sass.logError))
+    .pipe(uglifycss({
+      "maxLineLen": 80,
+      "uglyComments": true
+    }))
     .pipe(gulp.dest('./dist/css'));
 };
 
+
+/********************
+ * XX Scss Files XX
+ ******************/
+
+/*************
+ * Js Files
+ ***********/
 function joinJSFiles() {
   return gulp.src('src/scripts/script.js') // Change 'src/main.js' to your main JavaScript file
     .pipe(replace(/\/\/\s*###([^\n]+?)\n/g, function (match, placeholder) {
@@ -37,18 +57,39 @@ function joinJSFiles() {
       }
       return match; // If file doesn't exist, return the original placeholder
     }))
+
     .pipe(gulp.dest('dist/js')); // Output directory for JavaScript files
 }
 
+gulp.task('uglifyjs', () => {
+  return gulp.src('dist/js/script.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/js'));
+});
+
+
+/******************
+ * XX Js Files XX
+ ****************/
+
+
+/***************
+ * Watch Task
+ *************/
 
 function watch() {
   gulp.watch("./src/**/*.html", series(buildHTML));
-  gulp.watch("./src/scss/**/*.scss", series(combilecss));
+  gulp.watch("./src/scss/**/*.scss", series(combileAndMinifyCSS));
   gulp.watch("./src/scripts/**/*.js", series(joinJSFiles));
 }
 
+
+/*******************
+ * XX Watch Task XX
+ ******************/
+
 // Build task to prepare for deployment
-gulp.task('build', gulp.series(buildHTML, combilecss, joinJSFiles));
+gulp.task('build', gulp.series(buildHTML, combileAndMinifyCSS, joinJSFiles, 'uglifyjs'));
 
 // Default task
 gulp.task('default', gulp.series('build'));
@@ -56,6 +97,6 @@ gulp.task('default', gulp.series('build'));
 
 exports.watch = watch;
 exports.buildHTML = buildHTML;
-exports.combilecss = combilecss;
+exports.combileAndMinifyCSS = combileAndMinifyCSS;
 exports.joinJSFiles = joinJSFiles;
 
